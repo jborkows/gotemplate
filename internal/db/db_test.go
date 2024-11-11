6a4908de91be5e6106093aafe29e50c5d16a7da4
jborkows <jacek.borkowskiit@gmail.com>
2024-11-11 11:34:58 +0100

    tooling

--- internal/db/db_test.go
+++ internal/db/db_test.go
@@ -44,37 +44,36 @@ func TestConnectionString(t *testing.T) {
 		t.Fatalf("failed to create temp file: %v", err)
 	}
 	defer os.Remove(tempFile.Name())
+	db, err := queries.NewDatabase(tempFile.Name())
 
-	dsn := fmt.Sprintf("%s?_journal_mode=WAL&_foreign_keys=ON&_cache_size=2000", tempFile.Name())
-	db, err := sql.Open("sqlite3", dsn)
 	if err != nil {
 		fmt.Printf("Error opening database: %v\n", err)
 		return
 	}
 	defer db.Close()
 
-	// Step 3: Test the database by creating a table
-	_, err = db.Exec(`CREATE TABLE test (id INTEGER PRIMARY KEY, name TEXT)`)
+	err = queries.Execute(context.Background(), db.DB, func(tx *sql.Tx) error {
+		var journalMode, foreignKeys string
+		var cacheSize int
+		db.QueryRow("PRAGMA journal_mode").Scan(&journalMode)
+		db.QueryRow("PRAGMA foreign_keys").Scan(&foreignKeys)
+		db.QueryRow("PRAGMA cache_size").Scan(&cacheSize)
+
+		fmt.Printf("Journal mode: %s\n", journalMode)
+		fmt.Printf("Foreign keys enabled: %s\n", foreignKeys)
+		fmt.Printf("Cache size: %d pages\n", cacheSize)
+		assert.Equal(t, "wal", journalMode)
+		assert.Equal(t, "1", foreignKeys)
+		assert.Equal(t, 2000, cacheSize)
+		return nil
+	})
+	db.Optimize()
+
 	if err != nil {
-		fmt.Printf("Error creating table: %v\n", err)
+		fmt.Printf("Quering database: %v\n", err)
 		return
 	}
 
-	fmt.Println("Table 'test' created successfully.")
-
-	// Step 4: Verify the journal mode and foreign keys settings
-	var journalMode, foreignKeys string
-	var cacheSize int
-	db.QueryRow("PRAGMA journal_mode").Scan(&journalMode)
-	db.QueryRow("PRAGMA foreign_keys").Scan(&foreignKeys)
-	db.QueryRow("PRAGMA cache_size").Scan(&cacheSize)
-
-	fmt.Printf("Journal mode: %s\n", journalMode)
-	fmt.Printf("Foreign keys enabled: %s\n", foreignKeys)
-	fmt.Printf("Cache size: %d pages\n", cacheSize)
-	assert.Equal(t, "wal", journalMode)
-	assert.Equal(t, "1", foreignKeys)
-	assert.Equal(t, 2000, cacheSize)
 }
 
 func TestMigrations(t *testing.T) {
---
+++ internal/db/tooling.go
@@ -0,0 +1,79 @@
+package db
+
+import (
+	"context"
+	"database/sql"
+	"fmt"
+	"log"
+	"time"
+)
+
+type Database struct {
+	*sql.DB
+}
+
+func NewDatabase(filePath string) (*Database, error) {
+
+	dsn := fmt.Sprintf("file:%s?_journal_mode=WAL&_foreign_keys=ON&_cache_size=2000&_busy_timeout=5000", filePath)
+	db, err := sql.Open("sqlite3", dsn)
+	if err != nil {
+		log.Printf("Error opening database: %v\n", err)
+		return nil, fmt.Errorf("While opening %w", err)
+	}
+
+	// Configure connection pool settings
+	db.SetMaxOpenConns(10)                  // Max open connections
+	db.SetMaxIdleConns(5)                   // Max idle connections
+	db.SetConnMaxLifetime(30 * time.Minute) // Max connection lifetime
+
+	// Verify connection
+	if err := db.Ping(); err != nil {
+		return nil, fmt.Errorf("While pinging %w", err)
+	}
+	log.Println("Database connected successfully.")
+	return &Database{db}, nil
+}
+
+func (d *Database) Optimize() error {
+	return Execute(context.Background(), d.DB, func(tx *sql.Tx) error {
+		_, err := tx.Exec("PRAGMA optimize")
+		return err
+	})
+}
+
+func (d *Database) Close() error {
+	return d.DB.Close()
+}
+func Execute(ctx context.Context, db *sql.DB, exec func(*sql.Tx) error) error {
+	_, err := RunTransaction(ctx, db, nil, func(tx *sql.Tx) (interface{}, error) {
+		return nil, exec(tx)
+	})
+	return err
+}
+
+func RunTransaction[T any](ctx context.Context, db *sql.DB, defaulValue T, exec func(*sql.Tx) (T, error)) (T, error) {
+	tx, err := db.Begin()
+	if err != nil {
+		return defaulValue, fmt.Errorf("failed to begin transaction: %w", err)
+	}
+
+	defer func() {
+		if r := recover(); r != nil {
+			tx.Rollback()
+			panic(r) // Re-panic after rollback
+		} else if err != nil {
+			tx.Rollback() // Rollback on error
+		}
+	}()
+
+	value, err := exec(tx)
+	if err != nil {
+		return defaulValue, fmt.Errorf("Executing exec %w", err)
+	}
+
+	if err := tx.Commit(); err != nil {
+		return defaulValue, fmt.Errorf("failed to commit transaction: %w", err)
+	}
+
+	return value, nil
+}
